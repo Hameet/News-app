45942fc6606c2a20ffeaf1c9ecd3a41e8903137e
Hameet <hameet.singh@hotmail.com>
2018-10-23 15:19:34 +1300

    Changed components to Functional

--- src/Search.js
+++ src/Search.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 
-class Search extends Component {
-  render () {
-    const { value, onChange } = this.props
-    return (
-      <form>
-        <input type='text' value={value} onChange={onChange} />
-      </form>
-    )
-  }
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+      {children} <input type='text' value={value} onChange={onChange} />
+    </form>
+  )
 }
 
 export default Search
